2db8522b5f2fb7a039ce0920a9e5fda4cf5640cb
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-03-30 18:35:52 -0400

    favicon, edited template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+/**
+* Do basic math.
+*/
+function doMathClicked() {
+    // input
+    const length = parseFloat(document.getElementById("length").value)
+    const width = parseFloat(document.getElementById("width").value)
+    const height = parseFloat(document.getElementById("height").value)
+   
+   
+    // process
+    const volumeOfPyramid = (length * width * height) / 3
+   
+   
+    // output
+    document.getElementById("volume").innerHTML =
+      "<p>The area is: " + volumeOfPyramid + " cm³. </p>"
+   }
+   
+   
+   
