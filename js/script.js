bc7f77ed8e06e44cca4f9ccba50388e716a388de
default <isaac.ip@stu.ocsb.ca>
2025-04-01 17:18:56 +0000

    cm to mm

--- js/script.js
+++ js/script.js
@@ -21,6 +21,6 @@ function doMathClicked() {
 
     // output
     document.getElementById("volume").innerHTML =
-      "<p>The area is: " + volumeOfPyramid.toFixed(2) + " cm³. </p>"
+      "<p>The area is: " + volumeOfPyramid.toFixed(2) + " mm³. </p>"
    }
   
